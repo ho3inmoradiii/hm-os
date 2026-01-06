b500e7feb53395f6f4cda063df22409041317360
Hossein Moradi <ho3inmoradi1998@gmail.com>
2026-01-07 02:26:50 +0330

    change email

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,7 +1,7 @@
 export const LINKS = {
     GITHUB_REPO: 'https://github.com/ho3inmoradiii',
     LINKEDIN: "https://www.linkedin.com/in/ho3inmoradiii",
-    EMAIL: "mailto:ho3inmoradi1998@gmail.com",
+    EMAIL: "mailto:contact@hmoradi.xyz",
     RESUME: "/files/hossein-moradi-frontend-engineer.pdf",
     FIRST_POST: "https://www.linkedin.com/posts/ho3inmoradiii_mvp-aepaebaesaepaezaesaepafy-aetaexahyaezahyaesabraetaevaedaewaes-activity-7406597411617280001-IdPL?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC98Q14BYX3qmJzez9iSMgXfR4Twlp65qt8",
     SECOND_POST: "https://www.linkedin.com/posts/ho3inmoradiii_aetaevaeuaexaebahyabraeuaezaetabraepaepaeaaepaez-activity-7393653502767431681-FCa1?utm_source=share&utm_medium=member_desktop&rcm=ACoAAC98Q14BYX3qmJzez9iSMgXfR4Twlp65qt8",
